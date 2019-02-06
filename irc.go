d01d9a73ad87af6639a48d19a45b8ad6a656b2ba
ttle28c <ttle28c@users.noreply.github.com>
2019-02-05 19:49:27 -0800

    Input form (#1)
    * Create models packages for DB interaction.
    
    * Create package models for DB interaction
    
    * Move templates to own folder
    
    * Use nested templates
    
    * Move templates and CSS to seperate folders
    
    * Add user form for suggesting songs
    
    * Add user form for suggestions
    
    * Adjust formatting
    
    * Changed CSS
    
    * Switch order of title and artist
    
    * Change order of items in Suggestion
    
    * Switch artist and track
    
    * Make artist and title smaller
    
    * Remove unneeded import

---
+++ irc.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"regexp"
+	"time"
+
+	"golang.org/x/oauth2"
+)
+
+var tks oauth2.TokenSource
+
+var (
+	ping    = regexp.MustCompile(`^PING :([^\r\n]+)\r\n$`)
+	privmsg = regexp.MustCompile(`^:([^\r\n @]+)![^\r\n @]+@[^\r\n @]+\.tmi\.twitch\.tv PRIVMSG [^\r\n ]+ :([^\r\n]+)\r\n$`)
+	youtube = regexp.MustCompile(`(?:youtube\.com/watch\?v=|youtu\.be/)([A-Za-z0-9_-]{11})`)
+)
+
+func irc() {
+	for {
+		conn, err := tls.Dial("tcp", "irc.chat.twitch.tv:6697", nil)
+		if err != nil {
+			log.Printf("%v (retrying in 10 seconds)", err)
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		t, err := tks.Token()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(conn, "PASS oauth:%s\r\n", t.AccessToken)
+		fmt.Fprintf(conn, "NICK %s\r\n", "felixphew")
+		fmt.Fprintf(conn, "JOIN #%s\r\n", "kathleen_lrr")
+		r := bufio.NewReader(conn)
+		for {
+			msg, err := r.ReadString('\n')
+			if err != nil {
+				log.Print(err)
+				break
+			}
+			if match := ping.FindStringSubmatch(msg); match != nil {
+				fmt.Fprintf(conn, "PONG :%s\r\n", match[1])
+				log.Printf("Sent PONG %s", match[1])
+			} else if match := privmsg.FindStringSubmatch(msg); match != nil {
+				if ytmatch := youtube.FindStringSubmatch(match[2]); ytmatch != nil {
+					if match[1] == "bravenewfavesbot" {
+						log.Printf("Ignored link %s: <%s> %s", ytmatch[1], match[1], match[2])
+					} else {
+						log.Printf("Found link %s: <%s> %s", ytmatch[1], match[1], match[2])
+						//err = models.AddSuggestion(match[1], ytmatch[1], match[2])
+						if err != nil {
+							log.Print(err)
+						}
+					}
+				} else {
+					log.Printf("<%s> %s", match[1], match[2])
+				}
+			} else {
+				log.Printf("Unhandled message: %s", msg)
+			}
+		}
+	}
+}
---
+++ models/suggestions.go
@@ -0,0 +1,48 @@
+package models
+
+type Suggestion struct {
+	ID          int
+	Artist      string
+	Title       string
+	Description string
+	URL         string
+	UserName    string
+}
+
+func GetAllSongs() ([]*Suggestion, error) {
+	rows, err := db.Query("SELECT title, artist, description, url, username FROM submissions;")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	sugs := make([]*Suggestion, 0)
+	for rows.Next() {
+		s := new(Suggestion)
+		err := rows.Scan(&s.Title, &s.Artist, &s.Description, &s.URL, &s.UserName)
+		if err != nil {
+			return nil, err
+		}
+		sugs = append(sugs, s)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return sugs, nil
+}
+
+func AddSuggestion(s Suggestion) error {
+	_, err := db.Exec("INSERT INTO submissions(title, artist, description, url, username) VALUES(?, ?, ?, ?, ?);", s.Title, s.Artist, s.Description, s.URL, s.UserName)
+	return err
+}
+
+func MarkAsPlayed(id int) error {
+	_, err := db.Exec(`INSERT INTO history(user, videoid, message, date) SELECT user, videoid, message, CURRENT_DATE FROM submissions WHERE id = ?1;
+	DELETE FROM submissions WHERE id = ?1`, id)
+	return err
+}
+
+func Delete(id int) error {
+	_, err := db.Exec("DELETE FROM submissions WHERE id = ?;", id)
+	return err
+}
